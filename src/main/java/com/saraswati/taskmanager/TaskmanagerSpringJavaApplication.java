edba2dd0bfdd06a1f105ec02ba473503fdc1b067
saraswatikumari <saraswati.kumari@oracle.com>
2024-07-28 00:24:32 +0530

    Initial commit

---
+++ src/main/java/com/saraswati/taskmanager/TaskmanagerSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.saraswati.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerSpringJavaApplication.class, args);
+    }
+
+}
